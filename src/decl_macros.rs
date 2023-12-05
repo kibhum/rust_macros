500c501facabf154c7d318a3a8476cd164a53829
kibethh <kibethh@gmail.com>
2023-12-06 00:56:23 +0300

    Added vec decl macro

--- src/decl_macros.rs
+++ src/decl_macros.rs
@@ -34,21 +34,35 @@
 #[cfg(test)]
 
 mod test {
-    macro_rules! mad_skills {
-        // ($x:expr) => {
-        //     format!("You sent an expression: {}", $x)
-        // };
-        ($x:ty) => {
-            match stringify!($x) {
-                "i32" => "You sent an i32 type".to_string(),
-                _ => "You sent an something else".to_string(),
-            }
+    // macro_rules! mad_skills {
+    //     // ($x:expr) => {
+    //     //     format!("You sent an expression: {}", $x)
+    //     // };
+    //     ($x:ty) => {
+    //         match stringify!($x) {
+    //             "i32" => "You sent an i32 type".to_string(),
+    //             _ => "You sent an something else".to_string(),
+    //         }
+    //     };
+    // }
+
+    macro_rules! my_vec {
+        ($($x:expr),+) => {
+           { let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )+
+            temp_vec
+        }
         };
     }
 
     #[test]
     fn test_declarative_macros() {
-        let some_var: String = mad_skills!(i32);
-        dbg!(some_var);
+        let mut x: Vec<i32> = vec![1];
+        let mut y: Vec<i32> = my_vec!(4);
+        dbg!(y);
+        // let some_var: String = mad_skills!(i32);
+        // dbg!(some_var);
     }
 }
