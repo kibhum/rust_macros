531ff4549c23bf2e3187f5057c325417a1488255
kibethh <kibethh@gmail.com>
2023-12-19 22:17:27 +0300

    Used Weak Ref

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -33,5 +33,39 @@ mod tests {
 
         dbg!(x.borrow());
         dbg!(y.borrow());
+
+        #[derive(Debug,Clone)]
+        struct House {
+            address_number: u16,
+            street: String,
+            furniture: RefCell<Vec<Rc<Furniture>>>,
+        }
+        #[derive(Debug,Clone)]
+        struct Furniture {
+            id: String,
+            description: String,
+            house: Weak<House>,
+        }
+
+        let mut house_1: Rc<House> = Rc::new(House {
+            address_number: 123,
+            street: "Coding avenue".to_string(),
+            furniture: RefCell::new(vec![]),
+        });
+
+        let table: Rc<Furniture> = Rc::new(Furniture {
+            id: "table1".to_string(),
+            description: "kitchen table".to_string(),
+            house: Rc::downgrade(&house_1),
+        });
+        let desk: Rc<Furniture> = Rc::new(Furniture {
+            id: "desk1".to_string(),
+            description: "office desk".to_string(),
+            house: Rc::downgrade(&house_1),
+        });
+
+        house_1.furniture.borrow_mut().push(Rc::clone(&table));
+        house_1.furniture.borrow_mut().push(Rc::clone(&desk));
+        dbg!(house_1);
     }
 }
