b08c4fcaefca69e8859f0d07578dcfdea9b6673a
kibethh <kibethh@gmail.com>
2024-06-14 01:48:27 +0300

    Reached new types

--- src/decl_macros.rs
+++ src/decl_macros.rs
@@ -1,4 +1,29 @@
 // MACRO CAPTURES
+/*
+
+block: a block expression, i.e. statements between curly braces
+expr: an expression, a very wide variety of things within Rust
+ident: an identifier or keyword. For example, the start of a function
+declaration (fn hello) has a keyword followed by an identifier, and we
+can capture them both by using ident twice
+item: things like structs, enums, imports ('use declarations')…
+lifetime: a Rust lifetime ('a)
+literal: a literal, like a number or a character
+meta: the content of an attribute, so Clone or rename = "true". You get
+a good idea of what an attribute might contain in later chapters
+pat: a pattern, 1 | 2 | 3 is one example
+pat_param: similar to pat, except it can have | as a separator. So the
+rule ($first:pat_param | $second:ident) will work, but
+($first:pat | $second:ident) tells you that | is not allowed after
+pat. This also means you need to do some extra work to parse 1 | 2 |
+3 with pat_param (as it see 3 separate tokens instead of one)
+path: a path, things like ::A::B::C, or `Self::method
+stmt: a statement, for example an assignment (let foo = "bar")
+tt: a TokenTree, see the above explanation
+ty: a type, e.g. String
+vis: a visibility modifier, pub comes to mind
+
+*/
 
 /* expr
     matches to a valid rust expression
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+#![feature(trace_macros)]
+#![feature(log_syntax)]
 mod decl_macros;
 mod proc_macro;
 mod smart_pointers;
+#[macro_use]
+mod greeting;
+
+use crate::greeting::base_greeting_fn;
+
+fn calculate_raise(first_name: String, _last_name: String, _age: i32, current_pay: i32) -> i32 {
+    if first_name == "Sam" {
+        current_pay + 1000
+    } else {
+        current_pay
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let greet = greeting!("Sam", "Heya");
+    println!("{}", greet);
+    let greet_with_default = greeting!("Sam");
+    println!("{}", greet_with_default);
 }
